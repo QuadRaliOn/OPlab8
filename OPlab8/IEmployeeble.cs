dce7f4e49e82a6a9d992cc1e8f857d75af13dd1d
QuadRaliOn <146704998+QuadRaliOn@users.noreply.github.com>
2024-05-24 10:09:05 +0300

    Add files via upload

---
+++ OPlab8/IEmployeeble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPlab8
+{
+    internal interface IEmployeeble
+    {
+        public void AddEmployee(Employee employee);
+
+        public void RemoveEmployee(Employee employee);
+    }
+}
---
+++ OPlab8/Unit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPlab8
+{
+    public class Unit : IEmployeeble
+    {
+        public string Name { get; set; }
+
+        public List<Employee> employees = new List<Employee>();
+        public List<Project> projects = new List<Project>();
+
+        public void SortEmployees()
+        {
+            employees.Sort();
+        }
+
+        public void SortProjects()
+        {
+            projects.Sort((a,b) => a.GetPrice().CompareTo(b.GetPrice()));
+        }
+
+        public int GetProjectsPrice()
+        {
+            int sum = 0;
+            foreach (var item in projects)
+            {
+                sum += item.GetPrice();
+            }
+            return sum;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void RemoveEmployee(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+    }
+}
